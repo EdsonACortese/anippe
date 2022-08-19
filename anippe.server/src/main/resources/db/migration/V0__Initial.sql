6f81a3073dc6620b328cf735f1175898e395762c
Luka Cavic <luka.cavic@gmail.com>
2022-08-19 09:32:22 +0200

    Updated first base migration

--- anippe.server/src/main/resources/db/migration/V0__Initial.sql
+++ anippe.server/src/main/resources/db/migration/V0__Initial.sql
@@ -28,11 +28,60 @@ CREATE TABLE IF NOT EXISTS users (
 	deleted_at timestamp without time zone
 );
 
-CREATE TABLE IF NOT EXISTS clients (
+CREATE TABLE IF NOT EXISTS roles (
 	id serial primary key,
 	code character varying,
-	first_name character varying,
-	last_name character varying,
+	name character varying,
+	organisation_id integer not null,
+	created_at timestamp without time zone,
+	updated_at timestamp without time zone,
+	deleted_at timestamp without time zone
+);
+
+CREATE TABLE IF NOT EXISTS projects (
+	id serial primary key,
+	code character varying,
+	name character varying not null,
+	organisation_id integer not null,
+	created_at timestamp without time zone,
+	updated_at timestamp without time zone,
+	deleted_at timestamp without time zone
+);
+
+CREATE TABLE IF NOT EXISTS tickets (
+	id serial primary key,
+	code character varying,
+	name character varying,
+	creator_id integer not null,
+    creator_type SMALLINT  not null,
+    status_id SMALLINT  not null,
+    priority_id SMALLINT  not null,
+    title character varying null,
+    content character varying null,
+	organisation_id integer not null,
+	created_at timestamp without time zone,
+	updated_at timestamp without time zone,
+	deleted_at timestamp without time zone
+);
+
+CREATE TABLE IF NOT EXISTS activity_log (
+	id bigserial primary key,
+	creator_id integer not null,
+	creator_type SMALLINT  not null,
+	related_id integer not null,
+    related_type SMALLINT  not null,
+    log_action character varying null,
+    content character varying null,
+	organisation_id integer not null,
+	created_at timestamp without time zone,
+	updated_at timestamp without time zone,
+	deleted_at timestamp without time zone
+);
+
+CREATE TABLE IF NOT EXISTS customers (
+	id serial primary key,
+	code character varying,
+	name character varying,
 	organisation_id integer not null,
 	created_at timestamp without time zone,
 	updated_at timestamp without time zone,
---
+++ anippe.shared/src/main/java/com/velebit/anippe/shared/constants/Constants.java
@@ -0,0 +1,10 @@
+package com.velebit.anippe.shared.constants;
+
+public class Constants {
+    public static class Related {
+        public static final int CUSTOMER = 1;
+        public static final int USER = 2;
+        public static final int TASK = 3;
+        public static final int ROLE = 4;
+    }
+}
